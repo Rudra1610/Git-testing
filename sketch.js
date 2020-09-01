d734add49a9a84f1f38d3375d7e6626b1967cb2e
Rudra1610 <rudrasakariya16@gmail.com>
2020-09-01 16:53:41 +0530

    New Log

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(200,200,100,PI/2);	
     bird = new Bird(100,100);
 
 }
@@ -58,6 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();	
 
     bird.display();
     platform.display();
